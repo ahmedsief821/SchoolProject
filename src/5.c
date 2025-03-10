76afecc1ac0b13a73bccefdbd007ffc2a4e3976c
ahmedsief821 <ahmedsief821@gmail.com>
2025-03-10 10:01:00 +0000

    Add 5.c

---
+++ src/5.c
@@ -0,0 +1,16 @@
+
+Here is some random C code:
+
+int main()
+{
+   int i;
+   float f;
+   char c = 'a';
+
+   for (i = 0; i < 10; i++) {
+      f = f + 1.5;
+      printf("%f\n", f);
+   }
+
+   return 0;
+}
