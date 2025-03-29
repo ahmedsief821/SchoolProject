1960c23896c4f8d04a7b02f81721bcef1c42fbc8
ahmedsief821 <ahmedsief821@gmail.com>
2025-03-29 19:58:16 +0000

    Add 19.c

---
+++ src/19.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int array[5]; // Array to store some integer values
+
+    printf("Enter 5 integers:\n");
+    for (int i = 0; i < 5; ++i) {
+        scanf("%d", &array[i]);
+    }
+
+    // Perform some operations on the array
+    // Example: Sort the array, find the maximum value
+    // ...
+
+    return 0;
+}
